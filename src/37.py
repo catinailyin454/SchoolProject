483b89fb0a5faeb63532ea76ec9aea059a4144f5
catinailyin454 <catinailyin454@gmail.com>
2025-05-24 03:55:12 +0000

    Add 37.py

---
+++ src/37.py
@@ -0,0 +1,6 @@
+import random
+def random_string(length):
+    letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return ''.join(random.choice(letters) for _ in range(length))
+
+random_string(10)
